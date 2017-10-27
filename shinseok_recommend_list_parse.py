07095b8d02a5af83166bba4258d1f29f093819ab
Hikai <bahyun.lee@gmail.com>
2017-10-27 10:37:25 +0900

    devel

--- shinseok_recommend_list_parse.py
+++ shinseok_recommend_list_parse.py
@@ -1,10 +1,27 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """
 Shinseok recommend req.
 
 @Hikai
 """
+import json
 import requests
 
-r = requests.get("http://a.bns.plaync.com/bnsapi/main/shop/displaygoods")
-print(r.text)
+class Parser_goods():
+	"""Recommend goods parser."""
+
+	def __init__(self):
+		"""Initialize method."""
+		self.url = "http://a.bns.plaync.com/bnsapi/main/shop/displaygoods"
+		self.data = self.parse_data()
+		print(json.loads(self.data.text))
+
+	def parse_data(self):
+		"""Parse goods data."""
+		req = requests.get(self.url)
+
+		return req
+
+if __name__ == "__main__":
+	goods = Parser_goods()
+  
